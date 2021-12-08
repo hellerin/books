134ec85cef6238edcd356c910d15aa89517c79a8
Hellerin <hellerinm@gmail.com>
2021-12-08 11:36:18 +0200

    Get books from localStorage

--- app.js
+++ app.js
@@ -1,57 +1,115 @@
 
-
-const form = document.querySelector("form")
+//User input form
+const form = document.querySelector("#book-form")
 form.addEventListener("submit", addBook)
 
+function addBook(e){
+    const book = bookInput.value;
+    console.log(book);
+    e.preventDefault();
+}
+
+//Book List
+    bookList = document.querySelector("#book-list")
+    bookList.addEventListener("click", delBook)
+
+    //Page reload
+    document.addEventListener("DOMContentLoaded", getBooks)
+
 // getBooks
-function getBooks(){
-    //get data from local storage
-    let books
-    if(localStorage.getItem("books") === null){
-        books = []
-    } else {
-        books = JSON.parse(localStorage.getItem("books"))
+    function getBooks(){
+        //get data from local storage
+        let books
+        if(localStorage.getItem("books") === null){
+            books = []
+        } else {
+            books = JSON.parse(localStorage.getItem("books"))
+        }
+        const tr = document.createElement("tr")
+        const trContent =`
+ 
+    <td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td><a href="#">X</a></td>`
+
+        tr.innerHTML = trContent
+
+        bookList = document.querySelector("#book-list")
+        console.log(trContent)
+        bookList.appendChild(tr)
+        })
+    }
+
+    //delBook
+    function delBook(event){
+        if(event.target.textContent === "x"){
+            if(confirm("Do you really want to delete this book?")){
+                let isbn = event.target.parentElement.previousElementSibling.textContent
+                let author = event.target.parentElement.previousElementSibling.previousElementSibling.textContent
+                let title = event.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+                const book = [title, author, isbn]
+                event.target.parentElement.parentElement.remove()
+                removeBook(book)
+            }
+        }
+    }
+
+    //removeBook)
+    function removeBook(book){
+        let books
+        if(localStorage.getItem("books") === null){
+            books = []
+        } else {
+            books = JSON.parse(localStorage.getItem("books"))
+        }
+        books.forEach(function(bookFormLS, bookIndex){
+            if(bookFromLS[0] === book[0] && bookFromLS[1] === book[1] && bookFromLS[2] === book[2]){
+                books.splice(taskIndex, 1)
+            }
+        })
+        localStorage.setItem("books", JSON.stringify(books))
     }
 
 //addBook function
 function addBook(event){
+//get form submit value from form input
+    const title = document.querySelector("#title").value
+    const author = document.querySelector("#author").value
+    const isbn = document.querySelector("#isbn").value
 
-    const book = document.querySelector("#book").value
+    //create tr DOM element
+    const tr = document.createElement("tr")
+    const trContent =`
+ 
+    <td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td><a href="#">X</a></td>`
 
-    const bookList = document.querySelector("ul");
+    tr.innerHTML = trContent
+    bookList.appendChild(tr)
+    //Save to LS
+    const book = [title, author, isbn]
+    saveBook(book)
 
-    const li = document.createElement("li")
-    // add css class
-    li.className = "collection-item"
-    // add text to element
-    const text = document.createTextNode(book)
-    li.appendChild(text)
-    // create link
-    const link = document.createElement("a")
-    // add css style
-    link.className = "secondary-content"
-    // add text
-    link.appendChild(document.createTextNode("x"))
-    // add href attribute
-    link.setAttribute("href", "#")
-    // add link to list items
-    li.appendChild(link)
-    // add li to bookList
-    bookList.appendChild(li)
-    // save book to localStorage
-    bookStorage(book)
     // clear form input value
-    document.querySelector("#book").value = ""
+    document.querySelector("#title").value = ""
+    document.querySelector("#author").value = ""
+    document.querySelector("#isbn").value = ""
     event.preventDefault()
 }
 
-//delBook
-function delBook(event){
-    if(event.target.textContent === "x"){
-        if(confirm("Do you really want to delete this book?")){
-            event.target.parentElement.remove()
-            let book = event.target.parentElement.textContent.slice(0, -1)
-            removeStorage(book)
+    //saveBook
+    function saveBook(book){
+        let books
+        if(localStorage.getItem("books") === null){
+            books = []
+        } else {
+            books = JSON.parse(localStorage.getItem("books"))
         }
+        books.push(book)
+        console.log(books)
+        localStorage.setItem("books", JSON.stringify(books))
     }
-}
+
